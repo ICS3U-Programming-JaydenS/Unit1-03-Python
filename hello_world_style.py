7b7ab5fba607c9e5e561c4d42aae3d9052caba1f
Jayden Smith <jayden.smith2@stu.ocsb.ca>
2025-02-11 18:00:10 +0000

    Added hello_world_style.py

---
+++ hello_world_style.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+#Created By: Jayden Smith
+#Date: Feb 11, 2025
+# This programs displays hello world with style.
+
+def main():
+    print("Hello World with style!")
+    print("Testing the function.")
+
+
+if __name__ == "__main__":
+    main()
